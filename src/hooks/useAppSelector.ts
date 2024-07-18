9c5ae59f12e882eb80e1a2b6878c6c914bf811c1
Юлия Герасименко <Julia.grid2000@gmail.com>
2024-07-18 22:39:44 +0300

    Builded app

--- src/hooks/useAppSelector.ts
+++
@@ -1,4 +0,0 @@
-import { TypedUseSelectorHook, useSelector } from 'react-redux';
-import { State } from '../store/interfaces';
-
-export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
